57b27247864a687a6d0617fe261e3ddc91b15fd0
Jaspreet Singh <36043453+jaspreet-like@users.noreply.github.com>
2020-11-26 00:38:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ee604_plugins",
+    packages=['ee604_plugins'],
+    version="0.3.2",
+    description='Plugins to download dataset and some supported functions',
+    author = 'Shashi Kant Gupta',
+    author_email = 'shashikg.iitk@gmail.com',
+)
